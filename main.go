91f4dc1e9bbf40f626337a7dbf5f0d47c640f7f5
will.pan <will.pan@framy.co>
2024-09-11 18:37:25 +0800

    update all

--- main.go
+++ main.go
@@ -2,23 +2,148 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
-	_ "github.com/lib/pq"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"log"
+	"net/http"
+	"os"
 )
 
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+var DB *sql.DB
+
+func initDB() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Can't find .env file")
+	}
+
+	host := os.Getenv("MYSQL_HOST")
+	user := os.Getenv("MYSQL_USER")
+	password := os.Getenv("MYSQL_PASSWORD")
+	dbname := os.Getenv("MYSQL_DB")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true", user, password, host, dbname)
+
+	DB, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Successfully connected to MySQL!")
+}
+
+func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
+	rows, err := DB.Query("SELECT id, name, email FROM users")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	users := []User{}
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.ID, &user.Name, &user.Email)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		users = append(users, user)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
+}
+
+func GetUserHandler(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	row := DB.QueryRow("SELECT id, name, email FROM users WHERE id = ?", id)
+
+	var user User
+	err := row.Scan(&user.ID, &user.Name, &user.Email)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, "User not found", http.StatusNotFound)
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+}
+
+func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	_, err = DB.Exec("INSERT INTO users (name, email) VALUES (?, ?)", user.Name, user.Email)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func UpdateUserHandler(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	_, err = DB.Exec("UPDATE users SET name = ?, email = ? WHERE id = ?", user.Name, user.Email, id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	_, err := DB.Exec("DELETE FROM users WHERE id = ?", id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
 func main() {
-	dsn := "host=localhost port=5432 user=postgres dbname=postgres password=password sslmode=disable"
-	db, err := sql.Open("postgres", dsn)
-	if err != nil {
-		log.Fatal("Failed to connect to the database:", err)
-	}
-	defer db.Close()
+	initDB()
 
-	err = db.Ping()
-	if err != nil {
-		log.Fatal("Database connection is not alive:", err)
-	}
+	r := mux.NewRouter()
+	r.HandleFunc("/users", GetUsersHandler).Methods("GET")
+	r.HandleFunc("/users/{id:[0-9]+}", GetUserHandler).Methods("GET")
+	r.HandleFunc("/users", CreateUserHandler).Methods("POST")
+	r.HandleFunc("/users/{id:[0-9]+}", UpdateUserHandler).Methods("PUT")
+	r.HandleFunc("/users/{id:[0-9]+}", DeleteUserHandler).Methods("DELETE")
 
-	fmt.Println("Connected to the database successfully!")
+	log.Println("Server is running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
